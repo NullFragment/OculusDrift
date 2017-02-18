60d697f14d136a6ac2cb12f7daa29e7cf19e1821
Kyle Salitrik <nullfragment@gmail.com>
2017-02-18 17:44:48 -0500

    Audio Implemented

--- OculusDrift/Assets/Scripts/SliderBehavior.cs
+++ OculusDrift/Assets/Scripts/SliderBehavior.cs
@@ -15,7 +15,18 @@ public class SliderBehavior : MonoBehaviour {
 
 	public Material boxMaterial;
 
-	public AudioListener audio;
+	AudioSource audioMusic;
+	AudioSource audioNoise;
+	AudioSource audioPinkNoise;
+
+	public AudioClip lowMain, midMain, highMain, low1, low2, low3, mid1, mid2, mid3, high1, high2, high3, pinkNoise;
+
+	private float musicVolume = 1f;
+	private float noiseVolume = .03f;
+	private float pinknoiseVolue = .03f;
+
+	private int musicState = 1;
+
 
 	// Use this for initialization
 	void Start ()
@@ -25,14 +36,33 @@ public class SliderBehavior : MonoBehaviour {
 		Skybox2.SetColor("_TintColor", SkyboxTint);
 		Skybox3.SetColor("_TintColor", SkyboxTint);
 
-
 		//colorSlider.onValueChanged.AddListener(delegate { UpdateColors(); });
+
+		audioMusic = gameObject.AddComponent<AudioSource>();
+			 audioNoise = gameObject.AddComponent<AudioSource>();
+			 audioPinkNoise = gameObject.AddComponent<AudioSource>();
+
+		audioMusic.clip = lowMain;
+		audioMusic.volume = musicVolume;
+		audioMusic.Play();
+
+		audioNoise.clip = low1;
+		audioNoise.volume = noiseVolume;
+		audioNoise.Play();
+
+		audioPinkNoise.clip = pinkNoise;
+		audioPinkNoise.volume = pinknoiseVolue;
+		audioPinkNoise.Play();
+
+
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
 		UpdateColors();
+
+		UpdateSounds();
 		var star1 = ShootingStar1.main;
 		var star2 = ShootingStar2.main;
 		var star3 = ShootingStar3.main;
@@ -91,12 +121,14 @@ public class SliderBehavior : MonoBehaviour {
 				boxMaterial.color = temp;
 			}
 
-
+			
 			RenderSettings.skybox = Skybox1;
 		}
 
 		if (SliderValue > .3333333333333f && SliderValue < .666666666666666f)
 		{
+
+
 
 			if (SliderValue > .3333333333333f && SliderValue < 0.45f)
 			{
@@ -120,6 +152,9 @@ public class SliderBehavior : MonoBehaviour {
 
 		if (SliderValue > .666666666666666f && SliderValue < 1f)
 		{
+
+
+
 			if (SliderValue > .666666666666666f)
 			{
 				float opacity =1 - SliderValue * 1.2f;
@@ -131,5 +166,113 @@ public class SliderBehavior : MonoBehaviour {
 		}
 
 		Debug.Log(SliderValue);
+	}
+
+	void UpdateSounds()
+	{
+		if (SliderValue == 0.33f && musicState == 1) // from low to mid
+		{
+			audioMusic.Stop();
+			audioNoise.Stop();
+
+			audioMusic.clip = midMain;
+			audioMusic.volume = musicVolume;
+			audioMusic.Play();
+
+			audioNoise.clip = mid1;
+			audioNoise.volume = noiseVolume;
+			audioNoise.Play();
+
+			audioNoise.clip = mid2;
+			audioNoise.volume = noiseVolume;
+			audioNoise.Play();
+
+			audioNoise.clip = mid3;
+			audioNoise.volume = noiseVolume;
+			audioNoise.Play();
+
+			musicState = 2;
+			SliderValue += 0.01f;
+		}
+
+		if (SliderValue == 0.33f && musicState == 2) // from mid to low
+		{
+			audioMusic.Stop();
+			audioNoise.Stop();
+
+			audioMusic.clip = lowMain;
+			audioMusic.volume = musicVolume;
+			audioMusic.Play();
+
+			audioNoise.clip = low1;
+			audioNoise.volume = noiseVolume;
+			audioNoise.Play();
+
+			audioNoise.clip = low2;
+			audioNoise.volume = noiseVolume;
+			audioNoise.Play();
+
+			audioNoise.clip = low3;
+			audioNoise.volume = noiseVolume;
+			audioNoise.Play();
+
+			musicState = 1;
+
+			SliderValue -= 0.01f;
+		}
+
+		if (SliderValue == 0.66f && musicState == 2) // from mid to high
+			{
+				audioMusic.Stop();
+				audioNoise.Stop();
+
+				audioMusic.clip = highMain;
+				audioMusic.volume = musicVolume;
+				audioMusic.Play();
+
+				audioNoise.clip = high1;
+				audioNoise.volume = noiseVolume;
+				audioNoise.Play();
+
+				audioNoise.clip = high2;
+				audioNoise.volume = noiseVolume;
+				audioNoise.Play();
+
+				audioNoise.clip = high3;
+				audioNoise.volume = noiseVolume;
+				audioNoise.Play();
+
+				musicState = 3;
+				SliderValue += 0.01f;
+			}
+
+			if (SliderValue == 0.66f && musicState == 3) // from high to mid
+			{
+				audioMusic.Stop();
+				audioNoise.Stop();
+
+				audioMusic.clip = midMain;
+				audioMusic.volume = musicVolume;
+				audioMusic.Play();
+
+				audioNoise.clip = mid1;
+				audioNoise.volume = noiseVolume;
+				audioNoise.Play();
+
+				audioNoise.clip = mid2;
+				audioNoise.volume = noiseVolume;
+				audioNoise.Play();
+
+				audioNoise.clip = mid3;
+				audioNoise.volume = noiseVolume;
+				audioNoise.Play();
+
+				musicState = 2;
+
+				SliderValue -= 0.01f;
+			}
+
+		
+
 	}
 }
