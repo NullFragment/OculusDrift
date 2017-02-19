2bdc37b1a01f9cd4571a7bb87a6f5ce138450bc3
Kyle Salitrik <nullfragment@gmail.com>
2017-02-19 08:37:58 -0500

    Morning

---
+++ OculusDrift/Assets/Scripts/fadeout.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class fadeout : MonoBehaviour {
+
+	public Image splashImage;
+	public string loadLevel;
+
+	private IEnumerator Start()
+	{
+		splashImage.canvasRenderer.SetAlpha(0.0f);
+
+		fadeIn();
+		yield return new WaitForSeconds(2.5f);
+
+		fadeOut();
+		yield return new WaitForSeconds(2.5f);
+		SceneManager.LoadScene(loadLevel);
+	}
+
+	void fadeIn() { splashImage.CrossFadeAlpha(1.0f, 1.5f, false); }
+	void fadeOut() { splashImage.CrossFadeAlpha(0.0f, 2.5f, false); }
+
+}
